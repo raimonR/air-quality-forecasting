71f23485f0be8937b28e0d8d7661f92a694b514e
raimonR <rroglans@stanford.edu>
2021-09-30 21:39:28 -0700

    Initial commit.

---
+++ build_dataset.py
@@ -0,0 +1,10 @@
+import numpy as np
+import pandas as pd
+from numpy.random import default_rng
+import matplotlib.pyplot as plt
+
+# Define random seed for numpy
+rng = default_rng(0)
+
+# Number of preceding days for AQ forecasting
+d = 1
---
+++ organize_aq_data.py
@@ -0,0 +1,51 @@
+import pandas as pd
+import requests as r
+import time
+
+
+# Function to convert json response from API into pandas dataframe with only pm2.5 and datetime
+def to_dataframe(data: list):
+    dfs = []
+    for k, point in enumerate(data):
+        s1 = pd.Series({'pm25': point['value'], 'date': point['date']['utc']})
+        dfs.append(s1)
+
+    output = pd.concat(dfs, join='inner', axis=1).transpose()
+    return output
+
+
+# Define set of AQ stations
+# 725 = Santiago, Chile ~ 94.46k data points
+# 2009 = San Francisco, USA ~ 82.34k data points
+# 2549 = Chennai, India ~ 31.79k data points
+# 3455 = Toulouse, France ~ 73.82k data points
+# 10753 = Melbourne, Australia ~ 30.50k data points
+# 9764 = Accra, Ghana ~ 32.75k data points
+aq_stations = [[725, 'Santiago'], [2009, 'SanFran'], [2549, 'Chennai'], [3455, 'Toulouse'],
+               [10753, 'Melbourne'], [9764, 'Accra']]
+
+dataframe = []
+for el in aq_stations:
+    # Define API query parameters
+    query = {'date_from': '2015-01-01T00:00:00+00:00', 'date_to': '2021-12-31T00:00:00+00:00', 'parameter': 'pm25',
+             'location_id': el[0], 'limit': 100000, 'sort': 'asc'}
+
+    # Query API and retry if 1st query fails
+    response = r.get("https://u50g7n0cbj.execute-api.us-east-1.amazonaws.com/v2/measurements", params=query)
+    if response.status_code != 200:
+        time.sleep(10.0)
+        response = r.get("https://u50g7n0cbj.execute-api.us-east-1.amazonaws.com/v2/measurements", params=query)
+
+    # Parse API response
+    response = response.json()['results']
+
+    # Convert API response as a dataframe
+    if response:
+        df_temp = to_dataframe(response)
+        dataframe.append(df_temp)
+
+    # Combine and save dataframe
+    dataframe = pd.concat(dataframe, axis=0, ignore_index=True)
+    dataframe.to_pickle(f'dataset/aq_data_{el[1]}.pkl.zip')
+
+    time.sleep(0.05)
