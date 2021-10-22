24457cc5a201b07ec002fe2a803efe644acf505a
raimonR <rroglans@stanford.edu>
2021-10-22 01:28:41 -0700

    updated generate_dataset_split.py with covariate timeseries generation.

--- generate_dataset_split.py
+++ generate_dataset_split.py
@@ -2,13 +2,13 @@ import pandas as pd
 import numpy as np
 import os
 import sklearn as skl
+from darts import TimeSeries
 
 
 def grouped_dataset_split(rng_int: int):
     rng = np.random.default_rng(rng_int)
 
     files = os.listdir('dataset/merged/')
-
     for f in files:
         if f == 'Abidjan_merged_dataset.pkl':
             df_temp = pd.read_pickle(f'dataset/merged/{f}')
@@ -48,5 +48,23 @@ def grouped_dataset_split(rng_int: int):
 def transfer_dataset_split(rng_int: int):
     rng = np.random.default_rng(rng_int)
 
+    files = os.listdir('dataset/merged/')
+    for f in files:
+        if f == 'Abidjan_merged_dataset.pkl':
+            df_temp = pd.read_pickle(f'dataset/merged/{f}')
+            df_temp = df_temp.fillna(0)
 
-grouped_dataset_split(0)
+            forecast = TimeSeries.from_dataframe(df_temp, value_cols='pm25', freq='1H')
+
+            covariates = TimeSeries.from_dataframe(df_temp, value_cols='LATITUDE', freq='1H')
+            columns = df_temp.columns[(df_temp.columns != 'pm25') & (df_temp.columns != 'LATITUDE')].to_list()
+            for column in columns:
+                # TODO: figure out how to make list of arrays as timeseries, i.e. the pressure, temperature,
+                #  and theta_v vertical profiles
+                covariates = covariates.stack(TimeSeries.from_dataframe(df_temp, value_cols=column, freq='1H'))
+
+            print('a')
+
+
+# grouped_dataset_split(0)
+transfer_dataset_split(0)
