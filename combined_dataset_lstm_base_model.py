8d2aa1c82c07ce950a9b5ac449dbb736a36c84b9
raimonR <rroglans@stanford.edu>
2021-10-25 02:03:39 -0700

    another small change to combined_dataset_lstm_base_model.py

--- combined_dataset_lstm_base_model.py
+++ combined_dataset_lstm_base_model.py
@@ -21,7 +21,7 @@ model.compile(optimizer=keras.optimizers.Adam(learning_rate=0.01), loss='mse')
 model.summary()
 
 # Start hyperparameter tuning with epochs
-epochs = [1, 10, 50, 100, 250, 500, 1000, 2000]
+epochs = [1, 10, 50, 100, 250, 500, 1000]
 batches = 4
 
 train_loss = np.zeros((len(epochs), 10))
