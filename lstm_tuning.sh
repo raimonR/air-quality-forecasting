1524f7d434bd4eeddd25a2126152bdeeaf456a25
rroglans <rroglans@stanford.edu>
2021-10-27 13:18:18 -0700

    removed dropout from lstm model during hyperparameter tuning, will readd if necessary

--- lstm_tuning.sh
+++ lstm_tuning.sh
@@ -2,11 +2,12 @@
 #
 #SBATCH --job-name=lstm_tuning
 #
-#SBATCH --time=10:00
+#SBATCH -p gpu
+#SBATCH --time=05:00:00
 #SBATCH --ntasks=1
 #SBATCH --cpus-per-task=1
 #SBATCH --mem-per-cpu=8G
-#
+#SBATCH --gpus=1
 
 module purge
 ml cuda/11.2
@@ -16,4 +17,4 @@ eval "$(conda shell.bash hook)"
 
 conda activate /home/groups/gorle/miniconda3/envs/tensorflow_env
 
-python #script goes here
+python combined_dataset_lstm_base_model.py
