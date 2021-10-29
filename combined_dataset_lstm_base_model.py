c916ca580bad2bf7978665d9a19b04a6a67fbdf1
raimonR <rroglans@stanford.edu>
2021-10-29 00:19:44 -0700

    combined_dataset_lstm_base_model.py corrected with fixed values since hyperparameter tuning is done, now onto tests. individual_dataset_lstm_base_model.py updated for preliminary validation tests

--- combined_dataset_lstm_base_model.py
+++ combined_dataset_lstm_base_model.py
@@ -16,43 +16,42 @@ test_set_y = np.load('dataset/lstm_dataset_splits/collective/test_set_y.npy')
 
 
 # Start hyperparameter tuning with epochs
-dropout_rate = np.linspace(0, 1, num=10, endpoint=False)
-weights = 1e-6
+dr = 0.1
+weights = 1e-1
 epochs = 300
 batches = 128
 repeats = 3
 
-train_loss = np.zeros((len(dropout_rate), repeats))
-val_loss = np.zeros((len(dropout_rate), repeats))
+train_loss = np.zeros((len([1]), repeats))
+val_loss = np.zeros((len([1]), repeats))
 historical_train_loss = []
 historical_val_loss = []
-for i, dr in enumerate(dropout_rate):
-    t0 = time.perf_counter()
-    for j in range(repeats):
-        opt = keras.optimizers.Nadam(learning_rate=0.001, beta_1=0.9, beta_2=0.999, epsilon=1e-07, name="Nadam")
-        callback = keras.callbacks.EarlyStopping(monitor='val_loss', patience=10)
+t0 = time.perf_counter()
+for j in range(repeats):
+    opt = keras.optimizers.Nadam(learning_rate=0.001, beta_1=0.9, beta_2=0.999, epsilon=1e-07, name="Nadam")
+    callback = keras.callbacks.EarlyStopping(monitor='val_loss', patience=10)
 
-        inputs = Input(shape=(train_set_x.shape[1], train_set_x.shape[2]))
-        lstm_out = Bidirectional(LSTM(units=64, return_sequences=True, dropout=dr, recurrent_dropout=dr,
-                                      kernel_regularizer=l2(weights), recurrent_regularizer=l2(weights)))(inputs)
-        lstm_out_2 = Bidirectional(LSTM(units=32, dropout=dr, recurrent_dropout=dr,
-                                        kernel_regularizer=l2(weights), recurrent_regularizer=l2(weights)))(lstm_out)
-        outputs = Dense(units=24)(lstm_out_2)
+    inputs = Input(shape=(train_set_x.shape[1], train_set_x.shape[2]))
+    lstm_out = Bidirectional(LSTM(units=64, return_sequences=True, dropout=dr, recurrent_dropout=dr,
+                                  kernel_regularizer=l2(weights), recurrent_regularizer=l2(weights)))(inputs)
+    lstm_out_2 = Bidirectional(LSTM(units=32, dropout=dr, recurrent_dropout=dr,
+                                    kernel_regularizer=l2(weights), recurrent_regularizer=l2(weights)))(lstm_out)
+    outputs = Dense(units=24)(lstm_out_2)
 
-        model = keras.Model(inputs=inputs, outputs=outputs)
-        model.compile(optimizer=opt, loss='mse')
-        model.summary()
+    model = keras.Model(inputs=inputs, outputs=outputs)
+    model.compile(optimizer=opt, loss='mse')
+    model.summary()
 
-        res = model.fit(x=train_set_x, y=train_set_y, validation_data=(dev_set_x, dev_set_y),
-                        epochs=epochs, batch_size=batches, callbacks=[callback])
-        historical_train_loss.append(res.history['loss'])
-        historical_val_loss.append(res.history['val_loss'])
-        train_loss[i, j] = res.history['loss'][-1]
-        val_loss[i, j] = res.history['val_loss'][-1]
+    res = model.fit(x=train_set_x, y=train_set_y, validation_data=(dev_set_x, dev_set_y),
+                    epochs=epochs, batch_size=batches, callbacks=[callback])
+    historical_train_loss.append(res.history['loss'])
+    historical_val_loss.append(res.history['val_loss'])
+    train_loss[:, j] = res.history['loss'][-1]
+    val_loss[:, j] = res.history['val_loss'][-1]
 
-    t1 = time.perf_counter()
-    print(f'Total time for {repeats} repeats:', (t1 - t0)/60)
-    print('Time for 300 epochs:', ((t1 - t0)/repeats)/60)
+t1 = time.perf_counter()
+print(f'Total time for {repeats} repeats:', (t1 - t0)/60)
+print('Time for 300 epochs:', ((t1 - t0)/repeats)/60)
 
 np.save('results/tuning/recurrent_drop_rate_tuning_training_loss', train_loss)
 np.save('results/tuning/recurrent_drop_rate_tuning_validation_loss', val_loss)
--- individual_dataset_lstm_base_model.py
+++ individual_dataset_lstm_base_model.py
@@ -6,9 +6,18 @@ from tensorflow import keras
 from keras.preprocessing.sequence import TimeseriesGenerator
 from keras.models import Sequential
 from keras.layers import Input, LSTM, Dense, Bidirectional
+from keras.regularizers import l2
+
+
+# Define hyperparameters
+dr = 0.1
+weights = 1e-1
+epochs = 300
+batches = 128
+repeats = 3
 
 files = os.listdir('dataset/lstm_dataset_splits/individual/')
-for f in files:
+for f in [files[0]]:
     train_set_x = np.load(f'dataset/lstm_dataset_splits/individual/{f}/train_set_x.npy')
     train_set_y = np.load(f'dataset/lstm_dataset_splits/individual/{f}/train_set_y.npy')
     dev_set_x = np.load(f'dataset/lstm_dataset_splits/individual/{f}/dev_set_x.npy')
@@ -16,13 +25,22 @@ for f in files:
     test_set_x = np.load(f'dataset/lstm_dataset_splits/individual/{f}/test_set_x.npy')
     test_set_y = np.load(f'dataset/lstm_dataset_splits/individual/{f}/test_set_y.npy')
 
-    inputs = Input(shape=(train_set_x.shape[1], train_set_x.shape[2]))
-    lstm_out = Bidirectional(LSTM(units=296, return_sequences=True))(inputs)
-    lstm_out = Bidirectional(LSTM(units=148))(lstm_out)
-    outputs = Dense(units=24)(lstm_out)
+    opt = keras.optimizers.Nadam(learning_rate=0.001, beta_1=0.9, beta_2=0.999, epsilon=1e-07, name="Nadam")
+    callback = keras.callbacks.EarlyStopping(monitor='val_loss', patience=10)
 
-    model = keras.Model(inputs=inputs, outputs=outputs)
-    model.compile(optimizer=keras.optimizers.Adam(learning_rate=0.01), loss='mse')
+    model = keras.Sequential()
+    model.add(Input(shape=(train_set_x.shape[1], train_set_x.shape[2])))
+    model.add(Bidirectional(LSTM(units=64, return_sequences=True, dropout=dr, recurrent_dropout=dr,
+                                 kernel_regularizer=l2(weights), recurrent_regularizer=l2(weights))))
+    model.add(Bidirectional(LSTM(units=32, dropout=dr, recurrent_dropout=dr,
+                                 kernel_regularizer=l2(weights), recurrent_regularizer=l2(weights))))
+    model.add(Dense(units=24))
     model.summary()
 
+    model.compile(optimizer=opt, loss='mse')
 
+    res = model.fit(x=train_set_x, y=train_set_y, validation_data=(dev_set_x, dev_set_y),
+        epochs=epochs, batch_size=batches, callbacks=[callback])
+
+
+    print('done')
