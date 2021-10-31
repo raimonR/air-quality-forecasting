e490010550bc263d0a3149f6bd7dd6b08ded0114
raimonR <rroglans@stanford.edu>
2021-10-30 17:38:52 -0700

    small bugfixes for combined_dataset_lstm_base_model.py

--- combined_dataset_lstm_base_model.py
+++ combined_dataset_lstm_base_model.py
@@ -1,7 +1,6 @@
 import time
 import pickle
 import numpy as np
-import matplotlib.pyplot as plt
 from sklearn.metrics import mean_squared_error, mean_absolute_error, mean_absolute_percentage_error
 import matplotlib.pyplot as plt
 import tensorflow as tf
@@ -25,25 +24,26 @@ batches = 128
 learning_rate = 1e-3
 repeats = 5
 
+opt = keras.optimizers.Nadam(learning_rate=learning_rate, beta_1=0.9, beta_2=0.999, epsilon=1e-07, name="Nadam")
+callback = keras.callbacks.EarlyStopping(monitor='val_loss', patience=10, restore_best_weights=True)
+
+# TODO: Decide if i should set recurrent dropout to 0 so that training time speeds up.
+inputs = Input(shape=(train_set_x.shape[1], train_set_x.shape[2]))
+lstm_out = Bidirectional(LSTM(units=64, return_sequences=True, dropout=dr, recurrent_dropout=dr,
+                              kernel_regularizer=l2(weights), recurrent_regularizer=l2(weights)))(inputs)
+lstm_out_2 = Bidirectional(LSTM(units=32, dropout=dr, recurrent_dropout=dr,
+                                kernel_regularizer=l2(weights), recurrent_regularizer=l2(weights)))(lstm_out)
+outputs = Dense(units=24)(lstm_out_2)
+
+model = keras.Model(inputs=inputs, outputs=outputs)
+model.compile(optimizer=opt, loss='mse')
+model.summary()
+
+
 historical_train_loss = []
 historical_val_loss = []
 t0 = time.perf_counter()
 for j in range(repeats):
-    opt = keras.optimizers.Nadam(learning_rate=lr, beta_1=0.9, beta_2=0.999, epsilon=1e-07, name="Nadam")
-    callback = keras.callbacks.EarlyStopping(monitor='val_loss', patience=10, restore_best_weights=True)
-
-    # TODO: Decide if i should set recurrent dropout to 0 so that training time speeds up.
-    inputs = Input(shape=(train_set_x.shape[1], train_set_x.shape[2]))
-    lstm_out = Bidirectional(LSTM(units=64, return_sequences=True, dropout=dr, recurrent_dropout=dr,
-        kernel_regularizer=l2(weights), recurrent_regularizer=l2(weights)))(inputs)
-    lstm_out_2 = Bidirectional(LSTM(units=32, dropout=dr, recurrent_dropout=dr,
-        kernel_regularizer=l2(weights), recurrent_regularizer=l2(weights)))(lstm_out)
-    outputs = Dense(units=24)(lstm_out_2)
-
-    model = keras.Model(inputs=inputs, outputs=outputs)
-    model.compile(optimizer=opt, loss='mse')
-    model.summary()
-
     res = model.fit(x=train_set_x, y=train_set_y, validation_data=(dev_set_x, dev_set_y),
         epochs=epochs, batch_size=batches, callbacks=[callback])
     historical_train_loss.append(res.history['loss'])
