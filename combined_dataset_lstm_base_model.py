24a91cdb2ee4217fee0b7a8435c9490fd16f8607
raimonR <rroglans@stanford.edu>
2021-10-30 17:22:13 -0700

    finalized hyperparameters and now testing on dataset split and generating results

--- combined_dataset_lstm_base_model.py
+++ combined_dataset_lstm_base_model.py
@@ -2,6 +2,8 @@ import time
 import pickle
 import numpy as np
 import matplotlib.pyplot as plt
+from sklearn.metrics import mean_squared_error, mean_absolute_error, mean_absolute_percentage_error
+import matplotlib.pyplot as plt
 import tensorflow as tf
 from tensorflow import keras
 from keras.layers import Input, LSTM, Dense, Bidirectional
@@ -20,53 +22,61 @@ dr = 0.1
 weights = 1e-1
 epochs = 300
 batches = 128
-repeats = 3
+learning_rate = 1e-3
+repeats = 5
 
-# test learning rate
-learning_rate = [1*10**-v for v in range(5)]
-
-
-train_loss = np.zeros((len(learning_rate), repeats))
-val_loss = np.zeros((len(learning_rate), repeats))
 historical_train_loss = []
 historical_val_loss = []
+t0 = time.perf_counter()
+for j in range(repeats):
+    opt = keras.optimizers.Nadam(learning_rate=lr, beta_1=0.9, beta_2=0.999, epsilon=1e-07, name="Nadam")
+    callback = keras.callbacks.EarlyStopping(monitor='val_loss', patience=10, restore_best_weights=True)
 
-for i, lr in enumerate(learning_rate):
-    t0 = time.perf_counter()
-    for j in range(repeats):
-        opt = keras.optimizers.Nadam(learning_rate=lr, beta_1=0.9, beta_2=0.999, epsilon=1e-07, name="Nadam")
-        callback = keras.callbacks.EarlyStopping(monitor='val_loss', patience=10)
+    # TODO: Decide if i should set recurrent dropout to 0 so that training time speeds up.
+    inputs = Input(shape=(train_set_x.shape[1], train_set_x.shape[2]))
+    lstm_out = Bidirectional(LSTM(units=64, return_sequences=True, dropout=dr, recurrent_dropout=dr,
+        kernel_regularizer=l2(weights), recurrent_regularizer=l2(weights)))(inputs)
+    lstm_out_2 = Bidirectional(LSTM(units=32, dropout=dr, recurrent_dropout=dr,
+        kernel_regularizer=l2(weights), recurrent_regularizer=l2(weights)))(lstm_out)
+    outputs = Dense(units=24)(lstm_out_2)
 
-        # TODO: Decide if i should set recurrent dropout to 0 so that training time speeds up.
-        inputs = Input(shape=(train_set_x.shape[1], train_set_x.shape[2]))
-        lstm_out = Bidirectional(LSTM(units=64, return_sequences=True, dropout=dr, recurrent_dropout=dr,
-                                      kernel_regularizer=l2(weights), recurrent_regularizer=l2(weights)))(inputs)
-        lstm_out_2 = Bidirectional(LSTM(units=32, dropout=dr, recurrent_dropout=dr,
-                                        kernel_regularizer=l2(weights), recurrent_regularizer=l2(weights)))(lstm_out)
-        outputs = Dense(units=24)(lstm_out_2)
+    model = keras.Model(inputs=inputs, outputs=outputs)
+    model.compile(optimizer=opt, loss='mse')
+    model.summary()
 
-        model = keras.Model(inputs=inputs, outputs=outputs)
-        model.compile(optimizer=opt, loss='mse')
-        model.summary()
+    res = model.fit(x=train_set_x, y=train_set_y, validation_data=(dev_set_x, dev_set_y),
+        epochs=epochs, batch_size=batches, callbacks=[callback])
+    historical_train_loss.append(res.history['loss'])
+    historical_val_loss.append(res.history['val_loss'])
 
-        res = model.fit(x=train_set_x, y=train_set_y, validation_data=(dev_set_x, dev_set_y),
-                        epochs=epochs, batch_size=batches, callbacks=[callback])
-        historical_train_loss.append(res.history['loss'])
-        historical_val_loss.append(res.history['val_loss'])
-        train_loss[i, j] = res.history['loss'][-1]
-        val_loss[i, j] = res.history['val_loss'][-1]
+t1 = time.perf_counter()
+print(f'Total time for {repeats} repeats:', (t1 - t0)/60)
+print('Time for 300 epochs:', ((t1 - t0)/repeats)/60)
 
-    t1 = time.perf_counter()
-    print(f'Total time for {repeats} repeats:', (t1 - t0)/60)
-    print('Time for 300 epochs:', ((t1 - t0)/repeats)/60)
+# forecast air quality
+test_res = model.predict(test_set_x)
 
-np.save('results/tuning/learning_rate_tuning_training_loss', train_loss)
-np.save('results/tuning/learning_rate_tuning_validation_loss', val_loss)
+# metrics
+mse = mean_squared_error(test_set_y, test_res)
+mae = mean_absolute_error(test_set_y, test_res)
+mpe = mean_absolute_percentage_error(test_set_y, test_res)
 
-with open('results/tuning/lstm_combined_training_learning_rate.pickle', 'wb') as file:
+print('Mean Squared Error: ', mse)
+print('Mean Absolute Error: ', mae)
+print('Mean Absolute Percentage Error: ', mpe)
+
+fig, ax = plt.subplots()
+ax.plot(test_set_y, label=r'$y$')
+ax.plot(test_set_y, label=r'$\hat{y}$')
+ax.set(xlabel=r'$\text{Time}$', ylabel=r'$PM_{2.5}$')
+# plt.show()
+fig.savefig('results/tests/combined_lstm/forecast_vs_true_plot.png')
+plt.close()
+
+with open('results/tests/combined_lstm/lstm_combined_training_learning_rate.pickle', 'wb') as file:
     pickle.dump(historical_train_loss, file, protocol=-1)
 
-with open('results/tuning/lstm_combined_validation_learning_rate.pickle', 'wb') as file:
+with open('results/tuning/combined_lstm/lstm_combined_validation_learning_rate.pickle', 'wb') as file:
     pickle.dump(historical_train_loss, file, protocol=-1)
 
 print('done')
