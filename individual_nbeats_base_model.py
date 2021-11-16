bd699b40cbf000f8a499184e7137ce4ccae451f5
raimonR <rroglans@stanford.edu>
2021-11-15 16:42:28 -0800

    verifying whether nbeats model layers are accesible through darts

--- individual_nbeats_base_model.py
+++ individual_nbeats_base_model.py
@@ -18,40 +18,43 @@ model = NBEATSModel(input_chunk_length=24, output_chunk_length=24, generic_archi
                     layer_widths=1, n_epochs=300, optimizer_cls=torch.optim.Adam,
                     optimizer_kwargs={'lr': 0.01, 'eps': 1e-7}, model_name='base_model', force_reset=True)
 
-files = os.listdir('dataset/transfer_learning/')
-for f in [files[0]]:
-    train_set = pd.read_pickle(f'dataset/transfer_learning/{f}/train_set.pkl')
-    dev_set = pd.read_pickle(f'dataset/transfer_learning/{f}/dev_set.pkl')
-    test_set = pd.read_pickle(f'dataset/transfer_learning/{f}/test_set.pkl')
-    
-    train_set = train_set.apply(pd.to_numeric, downcast='float')
-    dev_set = dev_set.apply(pd.to_numeric, downcast='float')
-    test_set = test_set.apply(pd.to_numeric, downcast='float')
-    
-    print(train_set.dtypes)
+for params in model.parameters():
+    print(params)
 
-    n = test_set.shape[0]
-
-    train_forecast = TimeSeries.from_dataframe(train_set, value_cols='pm25', freq='1H')
-    dev_forecast = TimeSeries.from_dataframe(dev_set, value_cols='pm25', freq='1H')
-    test_forecast = TimeSeries.from_dataframe(test_set, value_cols='pm25', freq='1H')
-
-    train_covar = TimeSeries.from_dataframe(train_set, value_cols='LATITUDE', freq='1H')
-    dev_covar = TimeSeries.from_dataframe(dev_set, value_cols='LATITUDE', freq='1H')
-    test_covar = TimeSeries.from_dataframe(test_set, value_cols='LATITUDE', freq='1H')
-    columns = train_set.columns[(train_set.columns != 'pm25') & (train_set.columns != 'LATITUDE')].to_list()
-    for column in columns:
-        train_covar = train_covar.stack(TimeSeries.from_dataframe(train_set, value_cols=column, freq='1H'))
-        dev_covar = dev_covar.stack(TimeSeries.from_dataframe(dev_set, value_cols=column, freq='1H'))
-        test_covar = test_covar.stack(TimeSeries.from_dataframe(test_set, value_cols=column, freq='1H'))
-
-    model.fit(series=train_forecast, past_covariates=train_covar, val_series=dev_forecast,
-              val_past_covariates=dev_covar, verbose=True)
-
-    forecast = model.predict(n=n, series=test_forecast, past_covariates=test_covar)
-
-    fig, ax = plt.subplots()
-    ax.plot(test_forecast.data_array().to_numpy())
-    ax.plot(forecast.values)
-    fig.savefig('test_plot_for_nbeats_model.png')
-    plt.close()
+# files = os.listdir('dataset/transfer_learning/')
+# for f in [files[0]]:
+#     train_set = pd.read_pickle(f'dataset/transfer_learning/{f}/train_set.pkl')
+#     dev_set = pd.read_pickle(f'dataset/transfer_learning/{f}/dev_set.pkl')
+#     test_set = pd.read_pickle(f'dataset/transfer_learning/{f}/test_set.pkl')
+#
+#     train_set = train_set.apply(pd.to_numeric, downcast='float')
+#     dev_set = dev_set.apply(pd.to_numeric, downcast='float')
+#     test_set = test_set.apply(pd.to_numeric, downcast='float')
+#
+#     print(train_set.dtypes)
+#
+#     n = test_set.shape[0]
+#
+#     train_forecast = TimeSeries.from_dataframe(train_set, value_cols='pm25', freq='1H')
+#     dev_forecast = TimeSeries.from_dataframe(dev_set, value_cols='pm25', freq='1H')
+#     test_forecast = TimeSeries.from_dataframe(test_set, value_cols='pm25', freq='1H')
+#
+#     train_covar = TimeSeries.from_dataframe(train_set, value_cols='LATITUDE', freq='1H')
+#     dev_covar = TimeSeries.from_dataframe(dev_set, value_cols='LATITUDE', freq='1H')
+#     test_covar = TimeSeries.from_dataframe(test_set, value_cols='LATITUDE', freq='1H')
+#     columns = train_set.columns[(train_set.columns != 'pm25') & (train_set.columns != 'LATITUDE')].to_list()
+#     for column in columns:
+#         train_covar = train_covar.stack(TimeSeries.from_dataframe(train_set, value_cols=column, freq='1H'))
+#         dev_covar = dev_covar.stack(TimeSeries.from_dataframe(dev_set, value_cols=column, freq='1H'))
+#         test_covar = test_covar.stack(TimeSeries.from_dataframe(test_set, value_cols=column, freq='1H'))
+#
+#     model.fit(series=train_forecast, past_covariates=train_covar, val_series=dev_forecast,
+#               val_past_covariates=dev_covar, verbose=True)
+#
+#     forecast = model.predict(n=n, series=test_forecast, past_covariates=test_covar)
+#
+#     fig, ax = plt.subplots()
+#     ax.plot(test_forecast.data_array().to_numpy())
+#     ax.plot(forecast.values)
+#     fig.savefig('test_plot_for_nbeats_model.png')
+#     plt.close()
