48f7fd4adc15dde10e20742fc1085d24a2e4a975
raimonR <rroglans@stanford.edu>
2021-11-22 14:54:34 -0800

    adding file for transfer_learning_model.py

---
+++ transfer_learning_model.py
@@ -0,0 +1,37 @@
+import os
+import time
+import csv
+from joblib import load
+from itertools import zip_longest
+import numpy as np
+import matplotlib.pyplot as plt
+import pandas as pd
+from sklearn.metrics import mean_squared_error, mean_absolute_error, mean_absolute_percentage_error
+import tensorflow as tf
+from tensorflow import keras
+from keras.models import Sequential
+from keras.layers import Input, LSTM, Dense, Bidirectional
+from keras.regularizers import l1_l2
+from tensorflow import autograph
+
+autograph.set_verbosity(0)
+
+
+@tf.autograph.experimental.do_not_convert
+def generate_inputs_outputs(data, n_past, n_horizon, batch_num, shift):
+    def make_batch(x):
+        return x.batch(length, drop_remainder=True)
+
+    def make_split(x):
+        return x[:-n_horizon], x[-n_horizon:, 0]
+
+    length = n_past + n_horizon
+    ds = tf.data.Dataset.from_tensor_slices(data)
+
+    ds = ds.window(length, shift=shift, drop_remainder=True)
+    ds = ds.flat_map(make_batch)
+
+    ds = ds.map(make_split)
+
+    ds = ds.batch(batch_num, drop_remainder=True)
+    return ds
