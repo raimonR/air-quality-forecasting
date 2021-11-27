35589a26e8e7a272a1961ab76971524dc930115b
raimonR <rroglans@stanford.edu>
2021-11-26 22:54:56 -0800

    big changes to transfer_learning_model.py, code flow is now correct

--- transfer_learning_model.py
+++ transfer_learning_model.py
@@ -37,19 +37,19 @@ def generate_inputs_outputs(data, n_past, n_horizon, batch_num, shift):
     return ds
 
 
+# Define hyperparameters and other parameters
+epochs = 1000
+learning_rate = 1e-2
+l1l2 = (0.0, 0.0)
+n_features = 468
+past = 48
+horizon = 24
+batch_numbers = 128
+
 north_list = ['Anchorage', 'Oakland', 'Prague', 'Dhaka', 'Abidjan']
 south_list = ['Melbourne', 'Santiago', 'Sao Paulo', 'Thembisa']
 os.makedirs('dataset/transfer_learning/neural_networks/', exist_ok=True)
 for n in range(1, len(north_list) + 1):
-    # Define hyperparameters and other parameters
-    epochs = 1000
-    learning_rate = 1e-2
-    l1l2 = (0.0, 0.0)
-    n_features = 468
-    past = 48
-    horizon = 24
-    batch_numbers = 128
-
     opt = keras.optimizers.Nadam(learning_rate=learning_rate, beta_1=0.9, beta_2=0.999, epsilon=1e-07, name="Nadam")
     early_stopping = keras.callbacks.EarlyStopping(monitor='val_loss', patience=100, restore_best_weights=True)
     reduce_lr = keras.callbacks.ReduceLROnPlateau(monitor='val_loss', factor=0.2, patience=100, min_lr=0.0001)
@@ -58,11 +58,12 @@ for n in range(1, len(north_list) + 1):
     model.add(Input(shape=(past, n_features)))
     model.add(Bidirectional(LSTM(units=128, return_sequences=True, activity_regularizer=l1_l2(l1l2[0], l1l2[1]))))
     model.add(Bidirectional(LSTM(units=128, return_sequences=True, activity_regularizer=l1_l2(l1l2[0], l1l2[1]))))
-    model.add(Bidirectional(LSTM(units=64, kernel_regularizer=l1_l2(l1l2[0], l1l2[1]))))
-    model.add(Dense(units=horizon))
+    model.add(Bidirectional(LSTM(units=64, kernel_regularizer=l1_l2(l1l2[0], l1l2[1])), name='transfer_learning'))
+    model.add(Dense(units=horizon, name='output'))
     model.summary()
 
     model.compile(optimizer=opt, loss='mse')
+
     for loc in north_list[:n]:
         train_sets = os.listdir(f'dataset/transfer_learning/{loc}/train_sets/')
         dev_sets = os.listdir(f'dataset/transfer_learning/{loc}/dev_sets/')
@@ -97,9 +98,9 @@ for n in range(1, len(north_list) + 1):
             t1 = time.perf_counter()
             print(f'Time for {early_stopping.stopped_epoch} epochs:', t1 - t0)
 
-            model.save(f'dataset/transfer_learning/neural_networks/tl_n{n}')
+    model.save(f'dataset/transfer_learning/neural_networks/tl_n{n}')
 
-            keras.backend.clear_session()
+    keras.backend.clear_session()
 
 os.makedirs('results/tests/transfer_learning/', exist_ok=True)
 networks = os.listdir('dataset/transfer_learning/neural_networks/')
@@ -107,7 +108,53 @@ files = os.listdir('dataset/transfer_learning/').remove('neural_networks')
 for n in networks:
     model = keras.models.load_model(f'dataset/transfer_learning/neural_networks/{n}')
 
+    for layer in model.layers:
+        layer.trainable = False
+
+    model.layers[-2].trainable = True
+    model.layers[-1].trainable = True
+
+    model.summary()
+    model.compile(optimizer=opt, loss='mse')
+
+    for idx, f in enumerate(files):
+        train_sets = os.listdir(f'dataset/transfer_learning/{f}/train_sets/')
+        dev_sets = os.listdir(f'dataset/transfer_learning/{f}/dev_sets/')
+
+        n = np.floor(len(train_sets)/len(dev_sets)).astype(int)
+        dev_sets = np.concatenate([dev_sets for i in range(n)]).tolist()
+        for i in range(len(train_sets)%len(dev_sets)):
+            dev_sets.append(dev_sets[i])
+
+        zip_sets = list(zip_longest(train_sets, dev_sets))
+        t0 = time.perf_counter()
+        for sets in zip_sets:
+            train_set = pd.read_pickle(f'dataset/transfer_learning/{f}/train_sets/{sets[0]}').to_numpy()
+            dev_set = pd.read_pickle(f'dataset/transfer_learning/{f}/dev_sets/{sets[1]}').to_numpy()
+
+            train_ds = generate_inputs_outputs(train_set, past, horizon, batch_numbers[idx], 1)
+            dev_ds = generate_inputs_outputs(dev_set, past, horizon, batch_numbers[idx], 1)
+
+            i = 0
+            while len(list(train_ds)) < 1:
+                train_ds = generate_inputs_outputs(train_set, past, horizon, batch_numbers[idx] - i, 1)
+                i += 1
+
+            i = 0
+            while len(list(dev_ds)) < 1:
+                dev_ds = generate_inputs_outputs(dev_set, past, horizon, batch_numbers[idx] - i, 1)
+                i += 1
+
+            res = model.fit(x=train_ds, validation_data=dev_ds, epochs=epochs, shuffle=False,
+                            callbacks=[early_stopping, reduce_lr])
+
+            t1 = time.perf_counter()
+            print(f'Time for {early_stopping.stopped_epoch} epochs:', t1 - t0)
+
+        print(f'Done with {f}')
+
     for f in files:
+        os.makedirs(f'results/tests/transfer_learning/{f}/', exist_ok=True)
         test_sets = os.listdir(f'dataset/transfer_learning/{f}/test_sets/')
         predictions_array = np.array([])
         true_array = np.array([])
@@ -144,14 +191,17 @@ for n in networks:
         ax[0].legend()
         ax[0].set(ylabel=r'$PM_{2.5}$')
         ax[1].set(xlabel='Measurements', ylabel=r'$|y - \hat{y}|$')
-        fig.savefig(f'results/tests/transfer_learning/forecast_plots_n{n}.png')
+        fig.savefig(f'results/tests/transfer_learning/{f}/forecast_plots_n{n}.png')
         plt.close()
 
         mse = mean_squared_error(true_array, predictions_array)
         mae = mean_absolute_error(true_array, predictions_array)
         mpe = mean_absolute_percentage_error(true_array, predictions_array)
         metrics = {'Mean Squared Error': mse, 'Mean Absolute Error': mae, 'Mean Absolute Percentage Error': mpe}
-        with open(f'results/tests/transfer_learning/error_metrics_total_n{n}.csv', 'w') as error_file:
+        with open(f'results/tests/transfer_learning/{f}/error_metrics_total_n{n}.csv', 'w') as error_file:
             w = csv.writer(error_file)
             for key, value in metrics.items():
                 w.writerow([key, value])
+
+
+    keras.backend.clear_session()
